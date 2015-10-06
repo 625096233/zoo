119a509d6fb3e01c2eaa6713f68bdba94f62fc2c
Waldemar <waldemar.rittscher@live.de>
2015-10-06 22:31:27 +0200

    Gulp not needed

--- zoo-panda/gulp/fonts.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-
-gulp.task('fonts', function() {
-    return gulp.src([
-        'node_modules/font-awesome/fonts/*'
-    ]).pipe(gulp.dest('src/main/resources/static/fonts'));
-});
