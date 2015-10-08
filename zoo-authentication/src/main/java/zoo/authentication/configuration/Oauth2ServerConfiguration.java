9d056083641cfbae111cc8a3b4b4ee7176865312
w.rittscher <w.rittscher@interactivemedia.net>
2015-10-08 14:31:44 +0200

    Initial authentication work

---
+++ zoo-authentication/src/main/java/zoo/authentication/configuration/Oauth2ServerConfiguration.java
@@ -0,0 +1,11 @@
+package zoo.authentication.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author w.rittscher
+ * @since 08.10.2015
+ */
+@Configuration
+public class Oauth2ServerConfiguration {
+}
